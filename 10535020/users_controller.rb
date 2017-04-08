d50d0669225c4578e6a21bf47fc9557881e9ba9f
Ann-Lin <ztlin62595@gmail.com>
2017-04-08 18:21:36 +0800

    Update users_controller.rb

--- 10535020/users_controller.rb
+++ 10535020/users_controller.rb
@@ -12,11 +12,11 @@
   def create 
     @user = User.new(user_params) 產生新的一筆資料
     respond_to do |format| 
-      if @user.save 如果儲存一筆資料
+      if @user.save 如果儲存一筆資料成功
         format.html { redirect_to @user, notice: 'User was successfully created.' } 資料連接到@user,notice顯示User was successfully created.
-        format.json { render :show, status: :created, location: @user } 
-      else
-        format.html { render :new }
+        format.json { render :show, status: :created, location: @user }  傳送show，狀態為創建，位置在user
+      else  儲存失敗
+        format.html { render :new }  
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -24,14 +24,14 @@
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
+  def update 更新資料
+    respond_to do |format| 
+      if @user.update(user_params) 如果更新完成，顯示User was successfully updated.
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      else 更新失敗
+        format.html { render :edit } 跳至編輯狀態
+        format.json { render json: @user.errors, status: :unprocessable_entity } 顯示存取失敗
       end
     end
   end
@@ -42,12 +42,12 @@
 
   private 
     # Use callbacks to share common setup or constraints between actions.
-    def set_user 
+    def set_user 用id設定成user
       @user = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params 
+    def user_params 清除多餘欄位，以防駭客更改資料庫欄位
       params.require(:user).permit(:name, :email, :password)
     end
 end
