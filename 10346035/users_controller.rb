549c055849540f57b7e5bf2df3e82ed53725cf59
jiangxiang0831 <a0912124179@gmail.com>
2017-04-09 21:36:16 +0800

    Create users_controller.rb

---
+++ 10346035/users_controller.rb
@@ -0,0 +1,80 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+  #User這個model做出一個新的實體，並存為@User實體變數，供view使用
+  # GET /users/1/edit
+  def edit
+  end
+  #建立一個edit的Action，如果執行的話就會去view裡面找edit.html.erb這個檔案
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  #建立一個@User實體變數，裡面放user_params收集來的東西
+  #如果成功，就顯示User was successfully created
+
+  
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  #如果更新成功的話，就會顯示User was successfully updated.
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+  #只允許name，、email、password這些參數通過，其他的都會無視
+end
